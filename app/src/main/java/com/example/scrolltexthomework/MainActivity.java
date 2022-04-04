c89064881a6462c92b7df804b22ae1b866f798b8
jungae2 <jungaesuraj10@gmail.com>
2022-04-04 11:10:27 +0545

    Homework Done

--- app/src/main/java/com/example/scrolltexthomework/MainActivity.java
+++ app/src/main/java/com/example/scrolltexthomework/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.scrollingtext;
+package com.example.scrolltexthomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/scrolltexthomework/ExampleUnitTest.java
+++ app/src/test/java/com/example/scrolltexthomework/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.scrollingtext;
+package com.example.scrolltexthomework;
 
 import org.junit.Test;
 
